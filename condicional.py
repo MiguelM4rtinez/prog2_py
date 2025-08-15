788931c82d1d3fe412acaf89fa2a3b6b1be6612c
MiguelMar <miguelangelmartinezm21@gmail.com>
2025-08-15 11:44:25 -0500

    PUSGH

--- condicional.py
+++ condicional.py
@@ -6,4 +6,19 @@ else:
 
 
 
-b 
+b = 0 
+
+if b > 0:
+    print("es mayor a cero")
+elif a > 0:
+    print("es menor a cero")
+else:
+    print("es cero")
+
+user = "namey811"
+acees_level = 3 
+
+if user == "admin" or acees_level == 1:
+    print("USUARIO ADMINISTRADOR")
+else:
+    print("USUARIO GENERAL")    
