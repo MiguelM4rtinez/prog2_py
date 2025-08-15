1100206d68f4d6e4ad5fc75b192e63a31c3b6e00
luis ibarra <luisdavidibarraalmario@gmail.com>
2025-08-15 11:15:49 -0500

    Primer commit

---
+++ 03_var.py
@@ -0,0 +1,17 @@
+nombre = "Miguel"
+apellido = "Martinez"
+edad = 20 
+casado = False 
+
+asignatura = ["programacion", "tics"]
+
+personal_info = {
+    "nombre" : "Miguel",
+    "apellido" : "Martinez",
+    "edad" : 20
+}
+print(type (nombre))
+print(type (edad))
+print(type (casado))
+print(type ())
+print(type (nombre))
